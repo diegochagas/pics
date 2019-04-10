02975cb3debc22bbc978be02d7529a65aee88718
Diego Rocha Chagas <drochachagas@gmail.com>
2019-04-10 11:47:56 -0300

    Updates

---
+++ precache-manifest.bcbe1365b7cca155066a52b5391c622e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9198ae3b868f769fc8ea",
+    "url": "/pics/static/js/runtime~main.7218180c.js"
+  },
+  {
+    "revision": "9a0e33e1ed8381557460",
+    "url": "/pics/static/js/main.23a4d790.chunk.js"
+  },
+  {
+    "revision": "460dc445c3363f3e842b",
+    "url": "/pics/static/js/2.2b8c5950.chunk.js"
+  },
+  {
+    "revision": "9a0e33e1ed8381557460",
+    "url": "/pics/static/css/main.10b9da84.chunk.css"
+  },
+  {
+    "revision": "1893ccbd170235007eb51a2c3ad8293d",
+    "url": "/pics/index.html"
+  }
+];
